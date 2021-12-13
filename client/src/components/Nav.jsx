226d24b71120d071a650aef0fb14105333aec9df
Ian Lovice <ianlovice@Ians-Air.attlocal.net>
2021-12-13 15:21:21 -0500

    search transition works

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -4,13 +4,26 @@ import '../assets/css/nav.css';
 import logo from '../assets/images/logo.png'
 
 const Nav = () => {
+  const handleFocus = (ev) => {
+    ev.target.className = "nav__items__searchbar__focus"
+  }
+
+  const handleBlur = (ev) => {
+    ev.target.className = "nav__items__searchbar";
+  }
+
   return (
     <div className="nav" >
       <div className="nav__items" >
         <Link className="nav__items__logo" to="/">
           <img src={logo} alt="logo" />
         </Link>
-        <div className="nav__items__searchbar" ></div>
+        <input 
+          className="nav__items__searchbar" 
+          type="search" placeholder="Search" 
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+        />
       </div>
       <div className="nav__bar-out" >
         <div className="nav__bar-out__bar-in"></div>
